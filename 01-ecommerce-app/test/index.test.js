860953e2911c5e420476197d4b6cbefb51a37660
brother nifty macbook air <brotherniftymacbookair@brothers-MacBook-Air.local>
2025-03-19 15:23:50 -0500

    this

--- 01-ecommerce-app/test/index.test.js
+++ 01-ecommerce-app/test/index.test.js
@@ -1,71 +1,64 @@
-import { expect, describe, test, jest } from '@jest/globals'
-import Payment from '../src/events/payment.js'
-import Marketing from '../src/observers/marketing.js'
-import Shipment from '../src/observers/shipment.js'
-import PaymentSubject from '../src/subjects/paymentSubject.js'
+import { expect, describe, test, jest } from "@jest/globals";
+import Payment from "../src/events/payment.js";
+import Marketing from "../src/observers/marketing.js";
+import Shipment from "../src/observers/shipment.js";
+import PaymentSubject from "../src/subjects/paymentSubject.js";
 
-
-
-describe('Test Suite for Observer Pattern', () => {
-  test('#PaymentSubject notify observers', () => {
-    const subject = new PaymentSubject()
+describe("Test Suite for Observer Pattern", () => {
+  test("#PaymentSubject notify observers", () => {
+    const subject = new PaymentSubject();
     const observer = {
-      update: jest.fn()
-    }
-    const data = 'hello world'
-    const expected = data
+      update: jest.fn(),
+    };
+    const data = "hello world";
+    const expected = data;
 
-    subject.subscribe(observer)
-    subject.notify(data)
+    subject.subscribe(observer);
+    subject.notify(data);
 
-    expect(observer.update).toBeCalledWith(expected)
-  })
-  test('#PaymentSubject should not notify unsubscribed observers', () => {
-    const subject = new PaymentSubject()
+    expect(observer.update).toBeCalledWith(expected);
+  });
+  test("#PaymentSubject should not notify unsubscribed observers", () => {
+    const subject = new PaymentSubject();
     const observer = {
-      update: jest.fn()
-    }
-    const data = 'hello world'
-    
-    subject.subscribe(observer)
-    subject.unsubscribe(observer)
+      update: jest.fn(),
+    };
+    const data = "hello world";
 
-    subject.notify(data)
+    subject.subscribe(observer);
+    subject.unsubscribe(observer);
 
-    expect(observer.update).not.toHaveBeenCalled()
+    subject.notify(data);
 
-  })
-  test('#Payment should notify subject after a credit card transaction', () => {
-    const subject = new PaymentSubject()
-    const payment = new Payment(subject)
-    
-    const paymentSubjectNotifierSpy = jest.spyOn(
-      subject,
-      subject.notify.name,
-    )
+    expect(observer.update).not.toHaveBeenCalled();
+  });
+  test("#Payment should notify subject after a credit card transaction", () => {
+    const subject = new PaymentSubject();
+    const payment = new Payment(subject);
 
-    const data = { userName: 'erick', id: 3}
-    payment.creditCard(data)
+    const paymentSubjectNotifierSpy = jest.spyOn(subject, subject.notify.name);
 
-    expect(paymentSubjectNotifierSpy).toBeCalledWith(data)
-  })
-  test('#All should notify subscribers after a credit card payment', () => {
-    const subject = new PaymentSubject()
-    const shiptment = new Shipment()
-    const marketing = new Marketing()
+    const data = { userName: "erick", id: 3 };
+    payment.creditCard(data);
 
-    const shiptmentUpdateFnSpy = jest.spyOn(shiptment, shiptment.update.name)
-    const marketingUpdateFnSpy = jest.spyOn(marketing, marketing.update.name)
+    expect(paymentSubjectNotifierSpy).toBeCalledWith(data);
+  });
+  test("#All should notify subscribers after a credit card payment", () => {
+    const subject = new PaymentSubject();
+    const shiptment = new Shipment();
+    const marketing = new Marketing();
 
-    subject.subscribe(shiptment)
-    subject.subscribe(marketing)
+    const shiptmentUpdateFnSpy = jest.spyOn(shiptment, shiptment.update.name);
+    const marketingUpdateFnSpy = jest.spyOn(marketing, marketing.update.name);
 
-    const payment = new Payment(subject)
-    const data = { id: 4, userName: 'mary'}
-    payment.creditCard(data)
+    subject.subscribe(shiptment);
+    subject.subscribe(marketing);
 
-    expect(shiptmentUpdateFnSpy).toBeCalledWith(data)    
-    expect(marketingUpdateFnSpy).toBeCalledWith(data)    
-  })
+    const payment = new Payment(subject);
+    const data = { id: 4, userName: "mary" };
+    payment.creditCard(data);
 
-})
+    expect(shiptmentUpdateFnSpy).toBeCalledWith(data);
+    expect(marketingUpdateFnSpy).toBeCalledWith(data);
+  });
+});
